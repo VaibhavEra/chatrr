f293e46b0afb6753023cbe4a11be9a697f1125a6
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-04-24 11:04:44 +0000

    feat: Add authentication middleware and protected routes - Create protectRoute middleware to verify JWT tokens - Check token validity and decode user information - Set up authentication routes with express Router - Implement route protection for profile updates

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -84,3 +84,5 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const updateProfile = async (req, res) => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
@@ -9,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
